bb61d4ade9db6e2be86f8fe748f6f4e53b00ecc1
Linerre <leon.errelin@outlook.com>
2022-01-13 10:37:48 +0800

    Add project.clj

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject orderly "0.1.0-SNAPSHOT"
+  :description ""
+  :url "https://github.com/Linerre/orderly"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :mirrors {"clojars" {:name "ustc"
+                       :url "https://mirrors.ustc.edu.cn/clojars"}}
+  :profiles {:dev {:plugins []
+                   :dependencies []
+                   :source-paths ["dev"]}})
