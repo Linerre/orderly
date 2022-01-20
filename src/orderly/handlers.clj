fab1f14501e7384833258756e5b2f8629f0d6182
Linerre <leon.errelin@outlook.com>
2022-01-20 14:48:04 +0800

    Finish basic routes for three pages

--- src/orderly/handlers.clj
+++ src/orderly/handlers.clj
@@ -3,23 +3,38 @@
   Most important includes router and wrappers"
   (:require
    [ring.util.http-response :as resp] ; ring-http-response
+   [ring.util.http-predicates :as pred]
    ;; ring-compatible module while core is for data (serverless) only
    [reitit.ring :as ritr]
-   [orderly.views :as vs]))
+   [selmer.parser :as slmp]))
 
-;; TODO
-;; [x] add a handler returning current IP
-;; [x] add a middleware to automatically reload the server
-;; [x] add a route returning another message
+(defn handle-home
+  "Render the home.html using selmer.parser/render-file"
+  [req]
+  (resp/ok (slmp/render-file "html/home.html"
+                    {:resip "localhost",
+                     :view1 "Reserves",
+                     :view2 "CDL",
+                     :view3 "Others",
+                     :ip (:remote-addr req)})))
 
-(defn handler-one [request]
+(defn handle-res
+  "Handler fn for the res route."
+  [req]
   (resp/ok
-   (str "<html><body>Halo your IP is: "
-     (:remote-addr request)
-     "</body></html>")))
+   (slmp/render-file "html/res.html" {:name "You"})))
 
-(defn handler-home [request]
-  (resp/ok (vs/render-home)))
+(defn handle-cdl
+  "Handler fn for the res route."
+  [req]
+  (resp/ok
+   (slmp/render-file "html/cdl.html" {:name "You"})))
+
+(defn handle-oth
+  "Handler fn for the res route."
+  [req]
+  (resp/ok
+   (slmp/render-file "html/other.html" {:name "You"})))
 
 (defn handler-two [request]
   {:status 200
@@ -28,17 +43,48 @@
 
 ;; routes
 (def my-routes
-  [["/" handler-home]
+  [["/" {:get render-home}]
+   ["/reserves" {:get handle-res}]
+   ["/cdl" {:get handle-cdl}]
+   ["/other" {:get handle-oth}]
+   ;; ["/cdl" (handler-page "cdl")]
+   ;; ["/others" (handler-page "others")]
    ["/ping" handler-two]
    ["/pong/:id" ::user-id]])
 
-;; (def my-router
-;;   (ritr/router my-routes))
+;; TODO
+;; [ ] Figure out why the following code doesn't work as expected
+(comment
+  "Requst (as a map) always gets passed to a route fn"
+  ["/reserves" {:name ::reserves
+              :file {:name "res"
+                     :type "html"
+                     :path "html/res.html"}
+              :love "clojure"
+              :hate "javascript"
+              :get (fn [{{:keys [name]} :file}]
+                     (resp/ok {:filename name}))}])
+;; expected: => {:filename res}
+;; got: => {:filename nil}
 
+;;; ring-handler takes two args
+(comment
+  ritr/ring-handler (router) (ring-default-handler))
+
+
+;; TODO
+;; [] Add templates for default-handler pages, not-found in particular
 (def my-handler
   (ritr/ring-handler
    (ritr/router my-routes)
-   (constantly {:status 404, :body "Oops Page Not Found"})))
+   (ritr/create-default-handler
+    {:not-found
+     (constantly (resp/not-found "404 - Page not found"))
+     :method-not-allowed
+     (constantly (resp/method-not-allowed "405 - Operations not allowed"))
+     :not-acceptable
+     (constantly (resp/not-acceptable "406 - Not acceptable"))
+     })))
 
 (comment
   (ritc/routes my-router)
