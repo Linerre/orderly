9109b3eea20ccf02e6b77138eca70f4adf0590ba
Linerre <leon.errelin@outlook.com>
2022-02-03 01:21:04 +0800

    Make first connection to Sheet

--- src/orderly/sheets.clj
+++ src/orderly/sheets.clj
@@ -1,11 +1,20 @@
 (ns orderly.sheets
   (:require
-   [google-apps-clj.credentials :as gcred]
+   [google-apps-clj.credentials :as gauth]
    [google-apps-clj.google-drive :as gdrive]
-   [google-apps-clj.google-sheets-v4 :as gshv4]))
+   [google-apps-clj.google-sheets-v4 :as gsheets]))
 
 ;; If having no idea what the syntax would look like,
 ;; cider will offer auto-completion options as you type
-(let [scopes [com.google.api.services.sheets.v4.SheetsScopes/SPREADSHEETS_READONLY]
-      creds  (gcred/default-credential scopes)]
-    (gshv4/get-sheet-info creds ""))
+
+;; Define a service to pass to other handlers
+;; Think of it as a connection request sent to Google API
+(def service
+  (let [scopes [com.google.api.services.sheets.v4.SheetsScopes/SPREADSHEETS_READONLY]
+        creds  (gauth/default-credential scopes)]
+    (gsheets/build-service creds)))
+
+(comment
+  (gsheets/get-sheet-titles
+   service
+   "1V_4ot76E8RSgjZ08JiZzv0bGQzT8q0YV3cOye8C7m68"))
