4fd0cb55b90bac307aae5d0e993cf3cdbbd6be44
Linerre <leon.errelin@outlook.com>
2022-02-03 15:15:33 +0800

    Succeed connecting and fetching
    Note the raw data needs to be pre-processed before being fetched

--- src/orderly/sheets.clj
+++ src/orderly/sheets.clj
@@ -10,11 +10,17 @@
 ;; Define a service to pass to other handlers
 ;; Think of it as a connection request sent to Google API
 (def service
-  (let [scopes [com.google.api.services.sheets.v4.SheetsScopes/SPREADSHEETS_READONLY]
-        creds  (gauth/default-credential scopes)]
+  (let
+      [scopes [com.google.api.services.sheets.v4.SheetsScopes/SPREADSHEETS_READONLY]
+       creds  (gauth/default-credential scopes)]
     (gsheets/build-service creds)))
 
 (comment
   (gsheets/get-sheet-titles
    service
    "1V_4ot76E8RSgjZ08JiZzv0bGQzT8q0YV3cOye8C7m68"))
+(comment
+  (gsheets/get-cell-values
+   service
+   "1V_4ot76E8RSgjZ08JiZzv0bGQzT8q0YV3cOye8C7m68"
+   ["local-orders-since-2020-3!A1:I11"]))
